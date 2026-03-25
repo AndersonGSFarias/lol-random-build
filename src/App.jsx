9bc20e3bca6fae2becab95dca9669500ee716219
Anderson <anderson.gfarias05@gmail.com>
2026-03-24 23:29:40 -0300

    Inclusão do primeiro componente dos resultados do botão

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { HeroSection } from "./components/HeroSection";
+import { HeroSection } from "./components/HeroSection/HeroSection";
+import { ChampionCard } from "./components/Result/ChampionCard";
 
 function App() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <section className="flex flex-col items-center gap-8.75 my-[2.188rem]">
+        <ChampionCard />
+      </section>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Result/ChampionCard.jsx
@@ -0,0 +1,45 @@
+// Card do Herói
+function ChampionCard() {
+  return (
+    // Background
+    <section
+      className="
+        relative
+        bg-[#252937] 
+        w-[18.75rem] 
+        h-[10.313rem] 
+        flex justify-start items-end
+        border-2 border-solid border-secundary 
+        rounded-[10px]
+        bg-cover bg-center
+        shadow-[inset_0_-10px_30px_5px_#000000]
+      "
+      style={{
+        backgroundImage: `url('https://wiki.leagueoflegends.com/en-us/images/Irelia_OriginalSkin.jpg?12499')`,
+      }}
+    >
+      <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent rounded-[10px]"></div>
+      {/* Sobreamento interno */}
+      <div className="relative z-10"></div>
+      {/* Informações  */}
+      <div className="flex gap-[0.75rem] p-[0.625rem]">
+        {/* Icone do champ */}
+        <div className="w-[2.625rem] h-[2.625rem] rounded-[10px] bg-secundary"></div>
+
+        <div>
+          {/* Titulo do champ */}
+          <h4 className="eb-garamond uppercase bg-linear-to-r from-third to-fourth bg-clip-text text-transparent font-bold text-[0.75rem] mt-1">Irelia</h4>
+
+          <div className="flex items-center gap-1">
+            {/* Icone da role */}
+            <img src="https://wiki.leagueoflegends.com/en-us/images/All_roles_icon.png?d9e6c" alt="" className="w-[1rem] " />
+            {/* Nome da role */}
+            <span className="Inter text-[1rem] text-white font-bold">Meio</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { ChampionCard };
